f34eb4f656f8c598a640e311095a038d9497468d
Chad Ridings <ChadRidings@users.noreply.github.com>
2025-05-20 20:12:38 -0700

    Update some styles on User component

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -7,15 +7,15 @@ const User = () => {
 
     return (
         <div className="flex flex-col items-center p-5">
-            <h1 className="text-3xl font-bold mb-5">User Details</h1>
+            <h1 className="text-3xl font-bold mb-5 text-teal-500">User Details</h1>
 
             <div className="flex flex-col mb-5">
                 {users && users.map((user: UserType) => (
                     <div className="flex flex-col mb-5" key={user.id}>
                         <h3 className="font-bold">{user.name}</h3>
                         <p>Phone: {user.phone}</p>
-                        <p>Email: {user.email ? user.email : "N/A"}</p>
-                        <p>Website: {user.website ? user.website : "N/A"}</p>
+                        <p>Email: {user.email ? <a href={`mailto:${user.email}`} className="text-sky-500 italic">{user.email}</a> : "N/A"}</p>
+                        <p>Website: {user.website ? <a href={user.website} className="text-sky-500 italic">{user.website}</a> : "N/A"}</p>
                     </div>
                 ))}
             </div>
