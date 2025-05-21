70e77342bba17e8b1e345307dd71920872708633
Chad Ridings <ChadRidings@users.noreply.github.com>
2025-05-20 22:01:44 -0700

    Functionality to toggle dark/light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import { UserContextProvider } from './providers/UserContextProvider';
 import User from './components/User';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(prev => !prev);
+  };
 
   return (
-    <UserContextProvider>
-      <User />
-    </UserContextProvider>
+    <>
+      <div className={darkMode ? 'dark' : 'light'}>
+        <div className="flex flex-row items-center justify-center bg-gray-100 dark:bg-gray-900 p-2">
+          <button
+            onClick={toggleDarkMode}
+            className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+          >
+            {darkMode ? 'Light Mode' : 'Dark Mode'}
+          </button>
+        </div>
+        
+        <UserContextProvider>
+          <User />
+        </UserContextProvider>
+      </div>
+    </>
   )
 }
 
